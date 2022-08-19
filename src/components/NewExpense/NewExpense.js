b296b2c3dafb47ab2b464348e45dbf3d2b7c71b8
Rupesh <imyrups@gmail.com>
2022-08-19 10:38:20 +0530

    rendered add new expense form conditionally

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,34 @@
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
+import { useState } from 'react';
 
 const NewExpense = (props) => {
+    const [showForm, setShowForm] = useState(false);
     const saveExpenseHandler = (expense) => {
         const expenseData = {
             ...expense,
             id: Math.random().toString()
         }
         props.onAddExpense(expenseData);
+        resetAddNewHandler();
+    }
+    const addNewExpenseHandler = () => {
+        setShowForm(true);
+    }
+    const resetAddNewHandler = () => {
+        setShowForm(false);
+    }
+    let formContent = (
+        <button onClick={addNewExpenseHandler}>Add new expense</button>
+    )
+    if(showForm) {
+        formContent = (
+            <ExpenseForm onReset={resetAddNewHandler} onSaveExpenseData={saveExpenseHandler}/>
+        )
     }
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseHandler}/>
+            {formContent}
         </div>
     )
 }
