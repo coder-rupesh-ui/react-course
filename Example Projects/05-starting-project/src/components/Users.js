1d7adcdcbce2a8282298378311b7c079bff9e8e3
Rupesh <imyrups@gmail.com>
2022-10-15 19:30:26 +0530

    refactored users comp to class based

--- Example Projects/05-starting-project/src/components/Users.js
+++ Example Projects/05-starting-project/src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component, useState } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,7 +9,44 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
-const Users = () => {
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true,
+      moreState: 'Test',
+      nested: {},
+      arr: []
+    };
+  }
+  toggleUsersHandler() {
+    this.setState((curState) => {
+      return {
+        showUsers: !curState.showUsers
+      }
+    });
+  }
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+/* const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
 
   const toggleUsersHandler = () => {
@@ -32,6 +69,6 @@ const Users = () => {
       {showUsers && usersList}
     </div>
   );
-};
+}; */
 
 export default Users;
