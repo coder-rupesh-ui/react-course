29204ee22d6b71eadcee1ef924e9fc78ab542b54
Rupesh <imyrups@gmail.com>
2022-10-15 21:35:27 +0530

    added user context

--- Example Projects/05-starting-project/src/components/UserFinder.js
+++ Example Projects/05-starting-project/src/components/UserFinder.js
@@ -2,14 +2,10 @@ import { Fragment, useState, useEffect, Component } from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
-
-const DUMMY_USERS = [
-    { id: 'u1', name: 'Max' },
-    { id: 'u2', name: 'Manuel' },
-    { id: 'u3', name: 'Julie' },
-];
+import UsersContext from '../store/users-context';
 
 class UserFinder extends Component {
+    static contextType = UsersContext;
     constructor() {
         super();
         this.state = {
@@ -24,13 +20,13 @@ class UserFinder extends Component {
 
     componentDidMount() {
         // suppose we did an http call and on success we updated the list.
-        this.setState({filteredUsers: DUMMY_USERS});
+        this.setState({filteredUsers: this.context.users});
     }
 
     componentDidUpdate(prevProps, prevState) {
         if (prevState.searchTerm !== this.state.searchTerm) {
             this.setState({
-                filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm))
+                filteredUsers: this.context.users.filter((user) => user.name.includes(this.state.searchTerm))
             });
         }
     }
