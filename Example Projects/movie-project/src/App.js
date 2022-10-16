977fd5a552a37f7eabdc8886b542371cb4d03be3
Rupesh <imyrups@gmail.com>
2022-10-16 16:01:52 +0530

    added api to load data

--- Example Projects/movie-project/src/App.js
+++ Example Projects/movie-project/src/App.js
@@ -1,31 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  function fetchMovies() {
+    fetch('http://swapi.dev/api/films/').then(res => {
+      return res.json();
+    }).then(data => {
+      console.log(data);
+      setMovies(data.results);
+    }).catch(err => {console.log('error in fetching', err)});
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
