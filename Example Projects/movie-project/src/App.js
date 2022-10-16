8c167b7b2a5991e249ec3ac1ef7b6d9348f84622
Rupesh <imyrups@gmail.com>
2022-10-16 22:09:15 +0530

    post req with firebase google

--- Example Projects/movie-project/src/App.js
+++ Example Projects/movie-project/src/App.js
@@ -2,26 +2,37 @@ import React, { useCallback, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMovies = useCallback(async ()=>{
+  const fetchMovies = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
       // const res = await fetch('http://swapi.dev/api/film/'); // wrong api just to imitate failure
-      const res = await fetch('http://swapi.dev/api/films/');
-      if(!res.ok) {
+      // const res = await fetch('http://swapi.dev/api/films/');
+      const res = await fetch('https://react-http-714a7-default-rtdb.firebaseio.com/movies.json');
+      if (!res.ok) {
         throw new Error('Something went wrong');
       }
       const data = await res.json();
-      setMovies(data.results);
+      const loadedMovies = [];
+      for(let key in data) {
+        loadedMovies.push({
+          episode_id: key,
+          title: data[key].title,
+          releaseDate: data[key].releaseDate,
+          opening_crawl: data[key].openingText
+        });
+      }
+      setMovies(loadedMovies);
     } catch (err) {
       setError(err.message);
-      
+
     }
     setIsLoading(false);
   }, []);
@@ -30,8 +41,23 @@ function App() {
     fetchMovies();
   }, [fetchMovies]);
 
+  async function addMovieHandler(movie) {
+    const res = await fetch('https://react-http-714a7-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = res.json();
+    console.log(data);
+  }
+
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler}></AddMovie>
+      </section>
       <section>
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
