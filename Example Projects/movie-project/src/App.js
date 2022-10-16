ca61129e53c65d13c37637e9b80eb8662e2fc305
Rupesh <imyrups@gmail.com>
2022-10-16 16:37:17 +0530

    handling error

--- Example Projects/movie-project/src/App.js
+++ Example Projects/movie-project/src/App.js
@@ -6,11 +6,23 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   async function fetchMovies() {
     setIsLoading(true);
-    const res = await fetch('http://swapi.dev/api/films/');
-    const data = await res.json();
-    setMovies(data.results);
+    setError(null);
+    try {
+      const res = await fetch('http://swapi.dev/api/film/'); // wrong api just to imitate failure
+      // const res = await fetch('http://swapi.dev/api/films/');
+      if(!res.ok) {
+        throw new Error('Something went wrong');
+      }
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (err) {
+      setError(err.message);
+      
+    }
     setIsLoading(false);
   }
 
@@ -21,7 +33,8 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>No Movies to show.</p>}
+        {!isLoading && movies.length === 0 && !error && <p>No Movies to show.</p>}
+        {!isLoading && error && <p>{error}</p>}
         {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
