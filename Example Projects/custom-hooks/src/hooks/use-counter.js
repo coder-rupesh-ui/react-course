537ba8698a7f0d79c3676f6a5565736178f0162e
Rupesh <imyrups@gmail.com>
2022-10-17 09:58:56 +0530

    configuring custom hook for backward counter comp

--- Example Projects/custom-hooks/src/hooks/use-counter.js
+++ Example Projects/custom-hooks/src/hooks/use-counter.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 
-const useCounter = () => {
+const useCounter = (forwards = true) => {
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCounter((count) => count + 1);
+            setCounter((count) => {
+                return forwards ? count + 1 : count -1;
+            });
         }, 1000);
 
         return () => clearInterval(interval);
-    }, []);
+    }, [forwards]);
 
     return counter;
 }
