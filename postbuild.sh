dc8299080b4d45dc8de20b54728c098feb8fa571
Rupesh <imyrups@gmail.com>
2022-11-27 16:07:34 +0530

    fixed issues with base url

--- postbuild.sh
+++ postbuild.sh
@@ -23,14 +23,3 @@ mkdir webapp
 # Move all matches to the directory './user/'
 echo '2/4 Move relevant files'
 find . | grep -Ev '^.$|^.\/user$|^.\/static\/.+' | xargs -I{} mv -v {} webapp
-# Browse into './user/' directory
-cd webapp
-# Find all files within the folder (not subfolders)
-# Replace string 'static/' with 'user/static/' on all files that match the 'find'
-# ('sed' requires one to create backup files on OSX, so we do that)
-echo '3/4 Replace file references'
-find . -type f -maxdepth 1 | LC_ALL=C xargs -I{} sed -i.backup -e 's,static/,webapp/static/,g' {}
-# Delete '*.backup' files created in the last process
-echo '4/4 Clean up'
-find . -name '*.backup' -type f -delete
-# Done
